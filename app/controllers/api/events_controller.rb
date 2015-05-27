6cd70669f291e498a940082fc8a87f6cc1cd1a40
Shu Haur Tang <shuhaur.tang@gmail.com>
2015-05-26 22:07:46 -0400

    updated javascript

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,7 +1,13 @@
 class API::EventsController < ApplicationController
   
   skip_before_action :verify_authenticity_token
+  after_action :cors_set_access_control_headers
 
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
 
   def index
     @events = Event.all
--- public/blocmetrics.js
+++ public/blocmetrics.js
@@ -1,18 +1,23 @@
-var blocmetrics.report = function(name) {
+function blocmetrics(name) {
   
   var _bm_event = {
-    name: name,
+    name: name
   }
 
   var _bm_request = new XMLHttpRequest();
-    _bm_request.open("POST", "http://localhost:3000/api/events", true);
-    _bm_request.setRequestHeader('Content-Type', 'application/json');
+  _bm_request.open("POST", "http://localhost:3000/api/events", true);
+  _bm_request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   // #5
   _bm_request.onreadystatechange = function() {
+
   };
   
-  _bm_request.send(JSON.stringify(_bm_event));
+  _bm_request.send(jQuery.param(_bm_event));
 }
+
+  blocmetrics("page view");
+
+  blocmetrics("comment");
 
 
 
